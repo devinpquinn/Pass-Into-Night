a1da072f992469f6892500c6dffa636502b128ba
Devin <devinpquinn4@gmail.com>
2025-09-17 18:33:00 -0400

    interim

---
+++ Assets/Scripts/DialogManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DialogManager : MonoBehaviour
+{
+    public TextMeshProUGUI dialogText;
+
+    public Image[] characterPortraits;
+    public Sprite[] characterSpritesInactive;
+    public Sprite[] characterSpritesActive;
+}
