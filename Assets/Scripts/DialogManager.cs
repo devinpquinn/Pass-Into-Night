24aa2e6512e1d341a5f58efd15c9e66cf137060e
Devin <devinpquinn4@gmail.com>
2025-10-11 12:05:56 -0400

    interim

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -54,7 +54,9 @@ public class DialogManager : MonoBehaviour
             dialogPanelOriginalScale = dialogPanel.localScale;
         }
         uiCamera = Camera.main;
+        
         // Debug dialog lines
+        /*
         dialogQueue.Enqueue("Waif: Hello, I am Waif.");
         dialogQueue.Enqueue("Interesting. Here is some descriptive text.");
         dialogQueue.Enqueue("Priestess: Now Priestess is speaking.");
@@ -62,6 +64,7 @@ public class DialogManager : MonoBehaviour
         dialogQueue.Enqueue("Warder: I am still speaking.");
         dialogQueue.Enqueue("Pilot waits patiently.");
         dialogQueue.Enqueue("Pilot: Ok, now it's my turn!");
+        */
 
         portraitScaleTimers = new float[characterPortraits.Length];
         portraitStartScales = new float[characterPortraits.Length];
@@ -75,7 +78,7 @@ public class DialogManager : MonoBehaviour
             portraitTargetScales[i] = 1f;
         }
 
-        ShowNextDialog();
+        //ShowNextDialog();
     }
 
     void Update()
